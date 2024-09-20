65de1b795a42adf1513536d37959b378e081a68e
Anthony Barnes <anthony@sireni.com>
2024-09-20 05:33:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 require('dotenv').config();
 const { OpenAI } = require("@langchain/openai");
 
-console.log(process.env.OPENAI_API_KEY);
+const model = new OpenAI({ 
+    openAIApiKey: process.env.OPENAI_API_KEY, 
+    temperature: 0,
+    model: 'gpt-4.0-mini'
+  });
+  
+  console.log(model);
